3821f512c19a1d05bbcf28fd5daf3bfde4f71b85
luojiaquan0320 <58420846@qq.com>
2019-11-02 10:32:21 +0800

    指出Oneindex后台“文件管理”链接

--- view/admin/layout.php
+++ view/admin/layout.php
@@ -50,7 +50,7 @@
       <div class="mdui-list-item-content">密码修改</div>
     </a>
 
-    <a href="https://onedrive.live.com/" class="mdui-list-item" target="_blank">
+    <a href="https://onedrive.live.com/" class="mdui-list-item" target="_blank"> <!-- 网盘链接 -->
       <i class="mdui-list-item-icon mdui-icon material-icons">&#xe2bf;</i>
       <div class="mdui-list-item-content">文件管理(onedrive)</div>
     </a>
